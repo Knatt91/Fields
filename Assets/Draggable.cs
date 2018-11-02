92a5bcd53ba2124fa82adc9aff4e9f5117a7ab29
Nolan <nolang92@gmail.com>
2018-11-02 11:16:16 -0400

    Click And drag fields
    An updated version of fields
    
    Infantry can be dragged to any zone as long as it is a battlefield of a homefront  (all battlefields and homefronts are assigned "P1HOMEFRONT)
    
    Terrain cards can go in terrain zones

---
+++ Assets/Draggable.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+    public Transform parentToReturnTo = null;
+
+    public enum CardType { INFANTRY, TERRAIN, ITEM }
+    public CardType typeOfCard = CardType.INFANTRY;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag");
+
+        //when the drag starts the card will set 'parentToReturnTo' to it's current parent
+        //If the card is dragged to a spot that is invaled, then the card will default to its origional parent
+        parentToReturnTo = this.transform.parent;
+        this.transform.SetParent(this.transform.parent.parent);
+
+        //Turns off raycasts in order for the mouse to know what zone its over. 
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        //Debug.Log("OnDrag");
+
+        this.transform.position = eventData.position;
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+
+        this.transform.SetParent(parentToReturnTo);
+
+        //when the player releases click and drag of card, then its blockRaycasts will be turned back on.
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+
+}
---
+++ Assets/DropZone.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IDropHandler {
+
+    public enum ZoneType { P1HOMEFRONT, P1BATTLEFIELD, P1HFTERRAIN, P1BFTERRAIN, P2HOMEFRONT, P2BATTLEFIELD, P2HFTERRAIN, P2BFTERRAIN }
+    //public enum CardType { INFANTRY, TERRAIN, ITEM }
+    public ZoneType typeOfZone = ZoneType.P1HOMEFRONT;
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log(eventData.pointerDrag.name + " was dropped on " + gameObject.name);
+
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+        if (d != null)
+        {
+            //if the card is of type INFANTRY than it can be dropped in "P1HOMEFRONT" type zone
+            if (d.typeOfCard == Draggable.CardType.INFANTRY && typeOfZone == ZoneType.P1HOMEFRONT)
+            {
+                d.parentToReturnTo = this.transform;
+            }
+            //if the card is of type TERRAIN than it can be dropped in any terrain type zone
+            if (d.typeOfCard == Draggable.CardType.TERRAIN && typeOfZone == ZoneType.P1HFTERRAIN || typeOfZone == ZoneType.P1BFTERRAIN || typeOfZone == ZoneType.P2BFTERRAIN || typeOfZone == ZoneType.P2HFTERRAIN)
+            {
+                d.parentToReturnTo = this.transform;
+            }
+        }
+    }
+}
